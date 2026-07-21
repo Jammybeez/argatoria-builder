ca92a1e48767404d575ebabf9f9adcf81bde1e9c
james <jimny1984@hotmail.com>
2026-07-21 12:22:25 +0100

    changed to postgres

--- src/server/better-auth/config.ts
+++ src/server/better-auth/config.ts
@@ -6,7 +6,7 @@ import { db } from "~/server/db";
 
 export const auth = betterAuth({
   database: prismaAdapter(db, {
-    provider: "sqlite", // or "sqlite" or "mysql"
+    provider: "postgresql",
   }),
   emailAndPassword: {
     enabled: true,
